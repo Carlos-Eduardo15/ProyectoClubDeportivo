632cc0ec243c411ac446ea845476aa5f77b01b3b
DerekEMGA <gonzalezaguilar.derek@gmail.com>
2024-02-09 20:03:11 -0600

    Cambios en el login

--- ClubDeportivo/Formularios/frmLogin.cs
+++ ClubDeportivo/Formularios/frmLogin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClubDeportivo.Formularios;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,13 @@ namespace ClubDeportivo
         public frmLogin()
         {
             InitializeComponent();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            frmMENU frmMenu = new frmMENU();
+            frmMenu.Show();
+            this.Hide();
         }
     }
 }
