e161d4084cb5fb4b52bdd09001c75f436f8a35df
Carlos <carloseduhh15@gmail.com>
2024-02-11 16:07:08 -0600

    Creación de sociosNI

---
+++ ClubDeportivo/Formularios/SociosForms/SocioNI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClubDeportivo.Formularios.SociosForms
+{
+    public partial class SocioNI : Form
+    {
+        private static SocioNI instancia = null;
+        public static SocioNI ventana_unica2()
+        {
+            if (instancia == null)
+            {
+                instancia = new SocioNI();
+                return instancia;
+            }
+            return instancia;
+        }
+
+        public SocioNI()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
