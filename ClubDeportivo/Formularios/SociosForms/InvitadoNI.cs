4e4a5457a5792a9b50dc4b719f75e9210ee1bc44
Carlos <carloseduhh15@gmail.com>
2024-02-11 16:06:40 -0600

    Creacion de invitadoNI

---
+++ ClubDeportivo/Formularios/SociosForms/InvitadoNI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClubDeportivo.Formularios.SociosForms
+{
+    public partial class InvitadoNI : Form
+    {
+        private static InvitadoNI instancia = null;
+        public static InvitadoNI ventana_unica1()
+        {
+            if(instancia == null)
+            {
+                instancia = new InvitadoNI();
+                return instancia;
+            }
+            return instancia;
+        }
+
+        public InvitadoNI()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
