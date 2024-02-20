2e21ca12fb2198e3c97ce7127b8aacbd9fc8b373
Carlos <carloseduhh15@gmail.com>
2024-02-20 09:58:26 -0600

    Pa que me jale a mi

--- ClubDeportivo/funciones/VGlobal.cs
+++ ClubDeportivo/funciones/VGlobal.cs
@@ -2,8 +2,8 @@
 
 public static class VGlobal 
 {
-    static string conexion ="Data Source=DerekGA;Initial Catalog=ClubDeportivo;Integrated Security=True;Encrypt=False;TrustServerCertificate=True";
-   // static string conexion1 = "Data Source=DerekGA;Initial Catalog=ClubDeportivo;Integrated Security=True;Encrypt=False;TrustServerCertificate=True";
+    //static string conexion1 ="Data Source=DerekGA;Initial Catalog=ClubDeportivo;Integrated Security=True;Encrypt=False;TrustServerCertificate=True";
+    static string conexion = "Server=localhost;Database=ClubDeportivo;Trusted_Connection=True";
     //static string conexion2 = "Data Source=DerekGA;Initial Catalog=ClubDeportivo;Integrated Security=True;Encrypt=False;TrustServerCertificate=True";
 
 
