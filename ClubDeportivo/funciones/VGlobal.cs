7d3b2b5499749dfb24fb758ea0dcaa8405c0a662
DerekEMGA <gonzalezaguilar.derek@gmail.com>
2024-03-05 09:52:05 -0600

    cambio para que funcione merge

--- ClubDeportivo/funciones/VGlobal.cs
+++ ClubDeportivo/funciones/VGlobal.cs
@@ -5,7 +5,7 @@ public static class VGlobal
     //static string conexion1 ="Data Source=DerekGA;Initial Catalog=ClubDeportivo;Integrated Security=True;Encrypt=False;TrustServerCertificate=True";
     static string conexion = "Server=localhost;Database=ClubDeportivo;Trusted_Connection=True";
     //static string conexion ="Data Source=Data Source=DerekGA;Initial Catalog=ClubDeportivo;Integrated Security=True;Encrypt=False;TrustServerCertificate=True";
-    static string conexion = "Data Source=LAPTOP-1U3DONHI;Initial Catalog=clubdeportivo;Integrated Security=True;Encrypt=False;TrustServerCertificate=True"; 
+   // static string conexion = "Data Source=LAPTOP-1U3DONHI;Initial Catalog=clubdeportivo;Integrated Security=True;Encrypt=False;TrustServerCertificate=True"; 
     //static string conexion2 = "Data Source=DerekGA;Initial Catalog=ClubDeportivo;Integrated Security=True;Encrypt=False;TrustServerCertificate=True";
 
 
